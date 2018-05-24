7c462ea2d1319b79187ec395caae85222c098193
Will Hess <willh@aloompa.com>
2018-05-23 23:44:55 -0500

    added a few nodes tied to an "add" button and added a "reset" button to reset the AR session

--- worldTracking/ViewController.swift
+++ worldTracking/ViewController.swift
@@ -12,10 +12,57 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
+        
+        
     }
+    
+    @IBAction func add(_ sender: Any) {
+
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+
+        let node = SCNNode()
+
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+
+        node.position = SCNVector3(0.2,0.3,-0.2)
+        boxNode.position = SCNVector3(0, -0.05, 0)
+        doorNode.position = SCNVector3(0,-0.02,0.053)
+
+        self.sceneView.scene.rootNode.addChildNode(node)
+
+        node.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+}
+
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/180}
 }
 
